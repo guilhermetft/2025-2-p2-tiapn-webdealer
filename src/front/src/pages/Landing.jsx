4ca578a96acc0fb04847fcef043e47edc7ebf6b9
guilhermetft <guiftf2007@gmail.com>
2025-11-26 07:38:39 -0300

    teste em outro pc

--- src/front/src/pages/Landing.jsx
+++ src/front/src/pages/Landing.jsx
@@ -5,26 +5,40 @@ export default function Landing() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h2>Bem-vindo ao WebDealer</h2>
-      <p>
-        Aqui você vai encontrar de tudo — desde gerenciamento de tarefas e
-        projetos até outras funcionalidades.
-      </p>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center space-y-6">
+        <h2 className="text-2xl font-bold">
+          Bem-vindo ao WebDealer
+        </h2>
 
-      <div className="bg-white p-8">
-        <Button 
-        variant="ghost"
-        onClick={() => navigate("/login")}>Entrar</Button>
-      </div>
+        <p className="text-gray-600 max-w-md">
+          Aqui você vai encontrar de tudo — desde gerenciamento de tarefas e
+          projetos até outras funcionalidades.
+        </p>
 
-      <div className="bg-white p-8">
-        <Button 
-        variant="ghost"
-        onClick={() => navigate("/cadastro")}>Cadastrar-se</Button>
+        <div className="bg-white p-8 rounded-xl shadow">
+          <Button
+            type="submit" 
+            className="default"
+            onClick={() => navigate("/login")}
+          >
+            Entrar
+          </Button>
+        </div>
+
+        <div className="bg-white p-8 rounded-xl shadow">
+          <Button
+            type="submit" 
+            className="default"
+            onClick={() => navigate("/cadastro")}
+          >
+            Cadastrar-se
+          </Button>
+        </div>
       </div>
     </div>
   );
 }
+
 
 
