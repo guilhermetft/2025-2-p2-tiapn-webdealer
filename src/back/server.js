76f5c1479390c95775850ba2c1878b9bbfa76b8c
guilhermetft <guiftf2007@gmail.com>
2025-12-17 11:52:07 -0300

    quase finalizado

--- src/back/server.js
+++ src/back/server.js
@@ -9,6 +9,7 @@ import equipesRoutes from "./routes/equipes.js";
 import tarefasRoutes from "./routes/tarefas.js";
 import calendarioRoutes from "./routes/calendario.js"; 
 import projetosRoutes from "./routes/projetos.js"; 
+import chatRoutes from "./routes/chat.js";
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.use("/", cadastroRoutes);
 app.use("/api", equipesRoutes);
 app.use("/api", tarefasRoutes);
 app.use("/api", calendarioRoutes); 
+app.use("/chat", chatRoutes);
 app.use(projetosRoutes);
 
 const port = process.env.PORT || 5000;
