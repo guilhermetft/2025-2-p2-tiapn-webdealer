822a2fd70959a60e3eb0055f2a154d76ce21729b
guilhermetft <guiftf2007@gmail.com>
2025-12-18 01:24:55 -0300

    tarefas rodando

--- src/front/src/pages/Tarefas.jsx
+++ src/front/src/pages/Tarefas.jsx
@@ -19,8 +19,8 @@ function Tarefas() {
     prazo_tarefa: ''
   });
 
-  const API_URL = 'http://localhost:5000/api/tarefas';
-  const API_USUARIOS = 'http://localhost:5000/api/usuarios';
+  const API_URL = 'https://backwebdealer.onrender.com/tarefas';
+  const API_USUARIOS = 'https://backwebdealer.onrender.com/usuarios';
 
   // Buscar tarefas e usuários
   useEffect(() => {
